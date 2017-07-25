c8b8bf4b2c5cded652dc11a5a457179725f15db2
Christian Zamora <christian.j.zamora@gmail.com>
2017-07-25 14:26:02 -0700

    getTrending broke

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,37 +9,16 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
 
-  searchQuery: string = '';
-  items: { text: string, system: string }[];
-  allItems: { text: string, system: string }[];
 
   constructor(public navCtrl: NavController, public http: Http) {
     this.initializeItems();
   }
 
-  initializeItems() {
-    this.http.get('http://superfanlove.herokuapp.com/api/celebrities/tags?query=').map(res => res.json()).subscribe(data => {
-      this.allItems = data;
+  getTrending() {
+    this.http.get('http://superfanlove.herokuapp.com/api/trending').map(res => res.json()).subscribe(data => {
+      this.trendingitems = data;
     });
   }
 
-  resetItems() {
-    this.items = this.allItems;
-  }
-
-  getItems(ev: any) {
-    // Reset items back to all of the items
-    this.resetItems();
-
-    // set val to the value of the searchbar
-    let val = ev.target.value;
-
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-      this.items = this.items.filter((item) => {
-        return (item.text.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
-    }
-  }
 
 }
