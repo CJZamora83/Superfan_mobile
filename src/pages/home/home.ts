b5aef9dad79bbe54d6a581de302a7d01809409cc
alschofield93 <alschofield93@gmail.com>
2017-07-31 22:25:32 -0400

    need to define trending

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,6 +9,20 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
 
+  trending: {
+    systemname: string,
+    username: string,
+    type: string,
+    link: string,
+    createdAt: string,
+    image: string,
+    video: string,
+    caption: string,
+    comments: number,
+    likes: number,
+    views: number
+  }[];
+
   constructor(public navCtrl: NavController, public http: Http) {
 
     this.initializeItems();
@@ -16,6 +30,7 @@ export class HomePage {
 
   initializeItems() {
     this.http.get('http://superfanlove.herokuapp.com/api/trending').map(res => res.json()).subscribe(data => {
+      console.log(data);
       this.trending = data;
     });
   }
