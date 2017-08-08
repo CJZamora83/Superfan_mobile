c78e6095e7e581a74d18dd80edb164884d693747
Christian Zamora <christian.j.zamora@gmail.com>
2017-08-08 07:37:03 -0700

    media provider - trending works

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Http } from '@angular/http';
+import { Media } from '../../providers/media/media';
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -9,120 +9,8 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
 
-  trending: {
-    systemname: string,
-    username: string,
-    type: string,
-    link: string,
-    createdAt: string,
-    image: string,
-    video: string,
-    caption: string,
-    comments: number,
-    likes: number,
-    views: number
-  }[];
+  constructor(public navCtrl: NavController, public mediaService: Media) {
 
-  mostRecent: {
-    systemname: string,
-    username: string,
-    type: string,
-    link: string,
-    createdAt: string,
-    image: string,
-    video: string,
-    caption: string,
-    comments: number,
-    likes: number,
-    views: number
-  }[];
-
-  twitterHome: {
-    systemname: string,
-    username: string,
-    type: string,
-    link: string,
-    createdAt: string,
-    image: string,
-    video: string,
-    caption: string,
-    comments: number,
-    likes: number,
-    views: number
-  }[];
-
-    gramHome: {
-    systemname: string,
-    username: string,
-    type: string,
-    link: string,
-    createdAt: string,
-    image: string,
-    video: string,
-    caption: string,
-    comments: number,
-    likes: number,
-    views: number
-  }[];
-
-    tubeHome: {
-    systemname: string,
-    username: string,
-    type: string,
-    link: string,
-    createdAt: string,
-    image: string,
-    video: string,
-    caption: string,
-    comments: number,
-    likes: number,
-    views: number
-  }[];
-
-
-  constructor(public navCtrl: NavController, public http: Http) {
-
-    this.getTrending();
-    this.getMostRecent();
-    this.getTwitterHome();
-    this.getGramHome();
-    this.getTubeHome();
   }
-
-  getTrending() {
-    this.http.get('http://superfanlove.herokuapp.com/api/trending').map(res => res.json()).subscribe(data => {
-      console.log(data);
-      this.trending = data;
-    });
-  }
-
-  getMostRecent() {
-    this.http.get('http://superfanlove.herokuapp.com/api/mostRecent').map(res => res.json()).subscribe(data => {
-      console.log(data);
-      this.mostRecent = data;
-    });
-  }
-
-  getTwitterHome() {
-    this.http.get('http://superfanlove.herokuapp.com/api/twitter/home').map(res => res.json()).subscribe(data => {
-      console.log(data);
-      this.twitterHome = data;
-    });
-  }
-
-  getGramHome() {
-    this.http.get('http://superfanlove.herokuapp.com/api/instagram/home').map(res => res.json()).subscribe(data => {
-      console.log(data);
-      this.gramHome = data;
-    });
-  }
-
-  getTubeHome() {
-    this.http.get('http://superfanlove.herokuapp.com/api/youtube/home').map(res => res.json()).subscribe(data => {
-      console.log(data);
-      this.tubeHome = data;
-    });
-  }
-
 
 }
