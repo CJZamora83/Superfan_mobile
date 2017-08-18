2e6c0bbc27862c76cb6d4741aa0cf20fa2b45ac4
alschofield93 <alschofield93@gmail.com>
2017-08-17 22:41:53 -0400

    i didnt need that extra conditional

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -36,12 +36,10 @@ export class SettingsPage {
     let val = ev.target.value;
 
     // if the value is an empty string don't filter the items
-    if (val != '') {
-      if (val && val.trim() != '') {
-        this.items = this.items.filter((item) => {
-          return (item.text.toLowerCase().indexOf(val.toLowerCase()) > -1);
-        })
-      }
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.text.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
     } else {
       this.items = [];
     }
